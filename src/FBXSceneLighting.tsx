2d36ab20f0b8723829b5298699b7a3d66aa363b4
jakkaphat <jakkaphat@casasoft.studio>
2021-12-25 23:07:31 +0700

    implement hdri and bloom fx

--- src/FBXSceneLighting.tsx
+++ src/FBXSceneLighting.tsx
@@ -1,4 +1,4 @@
-import {  AmbientLight, CubeTextureLoader, LightProbe, Material, PerspectiveCamera, PMREMGenerator, PointLight, PointLightHelper, Scene, TextureLoader, Vector2, WebGLRenderer } from "three"
+import {  ACESFilmicToneMapping, AmbientLight, CubeTextureLoader, LightProbe, Material, Mesh, MeshPhysicalMaterial, PCFSoftShadowMap, PerspectiveCamera, PMREMGenerator, PointLight, PointLightHelper, Scene, SpotLight, SpotLightHelper, TextureLoader, UnsignedByteType, Vector2, WebGLRenderer, WebGLRenderTarget } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
@@ -7,6 +7,7 @@ import { LightProbeGenerator } from 'three/examples/jsm/lights/LightProbeGenerat
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { CanvasSize } from "./JSRender"
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
 
 export class FBXSceneLighting {
     private canvas: HTMLElement
@@ -18,6 +19,10 @@ export class FBXSceneLighting {
     controls: OrbitControls
 
     composer: EffectComposer
+
+    renderPass: RenderPass
+    
+    bloompass: UnrealBloomPass
 
     constructor(canvas: HTMLElement) {
         this.canvas = canvas
@@ -43,26 +48,31 @@ export class FBXSceneLighting {
     }
 
     init() {
-        this.renderer = new WebGLRenderer({canvas:this.canvas})
+        this.renderer = new WebGLRenderer({canvas:this.canvas, antialias: true})
         this.renderer.setSize(this.windowsize.width,this.windowsize.height)
+        this.renderer.toneMapping = ACESFilmicToneMapping
+        this.renderer.toneMappingExposure = 1
         this.renderer.physicallyCorrectLights = true
+        this.renderer.shadowMap.type = PCFSoftShadowMap
         this.renderer.shadowMap.enabled = true
-
-        /**
-         * Post Processing
-         */
-        const bloompass = new UnrealBloomPass(new Vector2(window.innerWidth, window.innerHeight), 1.5,0.4,0.85)
-        bloompass.threshold = .8
-        bloompass.strength = 10
-        bloompass.radius = 1
-
-        this.composer = new EffectComposer(this.renderer)
-        this.composer.addPass(bloompass)
+        //this.renderer.shadowMap.autoUpdate = true
 
         this.scene = new Scene()
         this.camera = new PerspectiveCamera(50,this.windowsize.width/this.windowsize.height,0.001,1000000)
         this.camera.position.set(0,4,5)
         this.camera.lookAt(0,0,0)
+
+        /**
+         * Post Processing
+         */
+        this.composer = new EffectComposer(this.renderer)
+        this.composer.setSize(this.windowsize.width, this.windowsize.height)
+
+        this.renderPass = new RenderPass(this.scene,this.camera)
+        this.bloompass = new UnrealBloomPass(new Vector2(window.innerWidth, window.innerHeight), 5, 1, 0.8)
+        
+        this.composer.addPass(this.renderPass)
+        this.composer.addPass(this.bloompass)
 
         // controls
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
@@ -75,18 +85,29 @@ export class FBXSceneLighting {
         /**
          *  Load HDRI
          */
-        //let envmaploader = new PMREMGenerator(this.renderer)
-        new RGBELoader().load('Ice_Lake_Env.hdr', HDRI =>{
+        let pmrem: PMREMGenerator = new PMREMGenerator(this.renderer)
+        pmrem.compileCubemapShader()
+
+
+        const _RBGELoader = new RGBELoader()
+        _RBGELoader.setDataType(UnsignedByteType)
+        _RBGELoader.load('Ice_Lake_Ref.hdr', HDRI =>{
             
-            this.scene.background = HDRI
+            const envMap: WebGLRenderTarget = pmrem.fromEquirectangular(HDRI)
+            HDRI.dispose()
+            pmrem.dispose()
+            this.scene.environment = envMap.texture
+
+            this.scene.background = envMap.texture
+            //this.scene.environment = envMap.texture
         })
-        const cubemap = new CubeTextureLoader()
-        cubemap.load(['Ice_Lake_Env.hdr'], cubemap =>{
-            const lightProbe = new LightProbe()
-            lightProbe.copy(LightProbeGenerator.fromCubeTexture(cubemap))
-            //lightProbe.intensity = 100
-            this.scene.add(lightProbe)
-        }) 
+        // const cubemap = new CubeTextureLoader()
+        // cubemap.load(['TropicalRuins_Env.hdr'], cubemap =>{
+        //     const lightProbe = new LightProbe()
+        //     lightProbe.copy(LightProbeGenerator.fromCubeTexture(cubemap))
+        //     //lightProbe.intensity = 100
+        //     this.scene.add(lightProbe)
+        // }) 
         
 
         /**
@@ -114,10 +135,23 @@ export class FBXSceneLighting {
         // //this.ListPointLightsHelper[0] = PointLight1Helper
         // this.scene.add(PointLight1Helper)
 
-        const ambientLight = new AmbientLight(0x404040)
-        ambientLight.intensity = 100
-        this.scene.add(ambientLight)
+        // const spotlight1 = new SpotLight( 0xffffff, 200, 20 )
+        // spotlight1.angle = Math.PI / 4
+        // spotlight1.position.set(3,15,0)
+        // spotlight1.lookAt(0,0,0)
+        // spotlight1.castShadow = true
+        // spotlight1.shadow.camera.near = 1
+        // spotlight1.shadow.camera.far = this.camera.far
+        // spotlight1.shadow.focus = 1
+        // spotlight1.shadow.mapSize.set(1024,1024)
+        // this.scene.add(spotlight1)
 
+        // const spotlight1Helper = new SpotLightHelper(spotlight1)
+        // this.scene.add(spotlight1Helper)
+
+        // const ambientLight = new AmbientLight(0x404040)
+        // ambientLight.intensity = 30
+        // this.scene.add(ambientLight)
 
         // FBX Loader
         const fbxLoader = new FBXLoader() 
@@ -129,6 +163,8 @@ export class FBXSceneLighting {
                 // {
                 //     child.material = Mat
                 // } 
+                
+                console.log(child)
 
                 const lightIntensity = .1
                 if(child instanceof PointLight)
@@ -144,12 +180,11 @@ export class FBXSceneLighting {
                     pointlight.position.set(child.position.x,child.position.y,child.position.z)
                     this.scene.add(pointlight)
                     
-                    
-                    const light_hepler = new PointLightHelper(child)
-                    light_hepler.update()
-                    this.scene.add(light_hepler)
-                    
+                    // const light_hepler = new PointLightHelper(child)
+                    // light_hepler.update()
+                    // this.scene.add(light_hepler)
                 }
+
                 // if(child instanceof SpotLight)
                 // {
                 //     child.intensity *= lightIntensity // decrase light intensity
@@ -169,8 +204,32 @@ export class FBXSceneLighting {
         const gltfLoader = new GLTFLoader()
         gltfLoader.load( 'S01.glb', s01 => {
             const model = s01.scene
-            model.receiveShadow = true
-            model.castShadow = true
+            model.traverse(geo => {
+                if(geo instanceof Mesh)
+                {
+                    console.log(geo.name)
+                    if(geo.name != "Mushroom_Quad")
+                    {
+                        geo.receiveShadow = true
+                    }
+                    else
+                    {
+                        const newMat: MeshPhysicalMaterial = new MeshPhysicalMaterial({map:geo.material.map})
+                        geo.material = newMat
+                        geo.material.roughness  = .8
+                        geo.material.metalness = .1
+                        geo.material.emissiveIntensity = 10
+                    }
+
+                    geo.castShadow = true
+
+
+                    console.log(geo.material)
+                    
+                }
+            })
+            //model.receiveShadow = true
+            //model.castShadow = true
             //model.scale.set(0.01,0.01,0.01)
             this.scene.add(model)
         })
@@ -182,9 +241,9 @@ export class FBXSceneLighting {
 
         this.controls.update()
 
-        this.composer.render()
-
         this.renderer.render(this.scene,this.camera)
+
+        this.composer.render()
     }
     
 }
