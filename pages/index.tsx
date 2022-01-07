c405bfe50758ee8df20c8060fbfe15e282650555
jakkaphat <jakkaphat@casasoft.studio>
2022-01-07 21:44:25 +0700

    revese commit - github hosting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,21 @@
-// import { MutableRefObject, useEffect, useRef } from "react"
-// import { Example1_Canvas } from "../src/example1Render"
-// import { JSRender } from "../src/JSRender"
-// import example1 from "./example1"
-
-// const Index = () => {
-
-//     useEffect(() => {
-//       document.title = "Render"
-//       new JSRender
-//     },[])
-  
-//     return (
-//         <>
-//         <canvas id="WebGL"></canvas>
-//         <button><a href='/example1'>Example1</a></button>
-//         </>
-//       )
-//   }
-  
-//   export default Index
-
 import { MutableRefObject, useEffect, useRef } from "react"
-import { KeychronQ2Render } from "../src/KeychronQ2Render"
+import { Example1_Canvas } from "../src/example1Render"
+import { JSRender } from "../src/JSRender"
+import example1 from "./example1"
 
-const KeychronQ2RenderPage = () => {
-
-    const canvas: MutableRefObject <HTMLCanvasElement> = useRef<HTMLCanvasElement>(null)
+const Index = () => {
 
     useEffect(() => {
-        document.title = "KeychronQ2",
-        new KeychronQ2Render(canvas.current)
+      document.title = "Render"
+      new JSRender
     },[])
-
-    return(
+  
+    return (
         <>
-			<style jsx global>{`
-				* {
-					margin: 0;
-					padding: 0;
-					box-sizing: border-box;
-				}
-
-				canvas {
-					display: block;
-				}
-			`}</style>
-
-            <canvas ref= { canvas}></canvas>
-
+        <canvas id="WebGL"></canvas>
+        <button><a href='/example1'>Example1</a></button>
         </>
-    )
-}
-
-export default KeychronQ2RenderPage
+      )
+  }
+  
+  export default Index
