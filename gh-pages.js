da084eebdb454d5ec3bdd371cdc0a3e51e26a66a
jakkaphat <jakkaphat@casasoft.studio>
2022-01-07 15:31:26 +0700

    publish pages

--- gh-pages.js
+++
@@ -1,9 +0,0 @@
-let ghpages = require('gh-pages');
-let moment = require('moment');
-ghpages.publish('out', {
-  branch: 'master',
-  dotfiles: true,
-  message: `🚀 Deploy [${ moment().format('DD/MM/YY') }]`,
-}, () => {
-  console.log('yayyy');
-});
